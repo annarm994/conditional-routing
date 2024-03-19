e177505709d7e17a2ad9ca66db6a25de1947b56a
annarm994 <155604262+annarm994@users.noreply.github.com>
2024-03-19 12:31:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Conditional Routing
+app.get('/foo', (req, res, next) => {
+    const random = Math.random();
+    if (random < 0.5) {
+        res.send("sometimes this");
+    } else {
+        next();
+    }
+});
+
+app.get('/foo', (req, res) => {
+    res.send("and sometimes that");
+});
+
+// Error Handling
+app.use((req, res, next) => {
+    res.status(404).send("404 - Not Found");
+});
+
+// Start the Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
